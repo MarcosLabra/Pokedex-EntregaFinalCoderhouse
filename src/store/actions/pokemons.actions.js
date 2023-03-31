be0a28f32a4eaa40eef2fb5e86f7fbf00d265f7f
MarcosLabra <mlabra.dev@gmail.com>
2023-03-31 13:05:40 -0300

    trato de hacer un get de pokemones al endpoint de la poke api

--- src/store/actions/pokemons.actions.js
+++ src/store/actions/pokemons.actions.js
@@ -3,8 +3,12 @@ export const GET_POKEMONS = 'GET_POKEMONS'
 export const getPokemons = () => {
     return async dispatch => {
         try {
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon')
-            const results = response?.results
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon/1', {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            console.log(response)
             dispatch({
                 type: GET_POKEMONS,
                 pokemons: results
