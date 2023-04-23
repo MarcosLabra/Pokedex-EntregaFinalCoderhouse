dd80d2ade95a5908bc124f58bec21ed50afae054
MarcosLabra <mlabra.dev@gmail.com>
2023-04-23 17:29:02 -0300

    publico el env para entrega final

---
+++ env.js
@@ -0,0 +1,11 @@
+import Constants from 'expo-constants';
+
+const ENV = {
+  API_KEY: 'AIzaSyAkcxmA_IlKznZP0cifAEu-u_JLKxXuw6g',
+};
+
+if (Constants.manifest.releaseChannel === 'production') {
+  ENV.API_KEY = 'AIzaSyAkcxmA_IlKznZP0cifAEu-u_JLKxXuw6g'
+}
+
+export default ENV;
